5018940621803447a0de1cbdebd22314f55c53b0
Ali Marjaninejad <alimarjaninejad@Alis-MacBook-Pro.local>
2020-04-03 01:17:26 -0700

    updated the figure displays and saved the results for cur3_1_

--- show_results_3phases_cur.py
+++ show_results_3phases_cur.py
@@ -2,8 +2,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from all_functions import *
 
-fig1, axes1 = plt.subplots(nrows=1, ncols=3, figsize=(9, 3))
-fig2, axes2 = plt.subplots(nrows=1, ncols=1, figsize=(3, 3))
+fig1, axes1 = plt.subplots(nrows=1, ncols=3, figsize=(12, 3.5))
+fig2, axes2 = plt.subplots(nrows=1, ncols=1, figsize=(6, 3.5))
 experiment_ID_base = 'cur3_1_'
 all_sensory_cases = [True, False]
 for use_sensory in all_sensory_cases:
@@ -24,25 +24,39 @@ for use_sensory in all_sensory_cases:
 	for ii in range(3):
 		axes1[ii].errorbar(x=np.arange(9), y=errors_all_mean[ii], yerr=errors_all_std[ii], capsize=2, animated=True, alpha=.3, color=color)
 		axes1[ii].plot(np.arange(9), errors_all_mean[ii],'--', alpha=.7, color=color)
-		axes1[ii].set_title('Task RMSE')
-		axes1[ii].set_xlabel('Task #')
+		if ii == 1:
+			axes1[ii].set_title('RMSE vs. Refinement # (learning)')
+		axes1[ii].set_xlabel('Refinement #')
 		axes1[ii].set_ylabel('RMSE')
-	
-	axes2.errorbar(x=np.arange(3), y=task_errors_mean, yerr=task_errors_std, capsize=2, animated=True, alpha=.3, color=color)
-	axes2.plot(np.arange(3), task_errors_mean,'--', alpha=.7, color=color)
-	axes2.set_title('Task RMSE')
-	axes2.set_xlabel('Task #')
-	axes2.set_ylabel('RMSE')
+		
+	if use_sensory:
+		x_shift=.25
+	else:
+		x_shift=0
+	for ii in range(3):
+		axes2.errorbar(ii+x_shift, y=task_errors_mean[ii], yerr=task_errors_std[ii], capsize=2, animated=True, alpha=.3, color=color)
+		axes2.bar(ii+x_shift, task_errors_mean[ii], width=0.45,alpha=.6, color=color)
+		axes2.set_title('RMSE vs. Task (test)')
+		axes2.set_xlabel('Task')
+		axes2.set_ylabel('RMSE')
 
-show_video = True
-use_sensory = False
+save_figures = False
+if save_figures:
+	dpi = 600
+	fig1.subplots_adjust(left=.06, bottom=.12, right=.96, top=.92, wspace=.30, hspace=.20)
+	fig1.savefig("./results/{}_figure1.png".format(experiment_ID_base), dpi=dpi)
+	fig2.subplots_adjust(bottom=.12, top=.92)
+	fig2.savefig("./results/{}_figure2.png".format(experiment_ID_base), dpi=dpi)
+
+show_video = False
+#use_sensory = False
 experiment_ID = experiment_ID_base+"wo_sensory"
 run_no = 0
 MuJoCo_model_names = ["tendon_quadruped_ws_onair.xml", "tendon_quadruped_ws_onfloor.xml", "tendon_quadruped_ws_onfloorloaded.xml"]
 if show_video:
 	for MuJoCo_model_name , ii in zip(MuJoCo_model_names, range(len(MuJoCo_model_names))):
 		tmp = test_a_task(MuJoCo_model_name, experiment_ID, run_no, use_sensory=use_sensory,  Mj_render=True)
-
 plt.show(block=True)
+
 
 #import pdb; pdb.set_trace()
