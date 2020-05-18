fddcbe90b6fbd9c40f1270178252dfed4dc3799d
Ali Marjaninejad <alimarjaninejad@Alis-MacBook-Pro.local>
2020-05-18 14:42:48 -0700

    updated show_results3phases_2tasks_cul_mulANN_par.py to show babbling vertical line

--- show_results_3phases_2tasks_cur_multiANN_par.py
+++ show_results_3phases_2tasks_cur_multiANN_par.py
@@ -12,11 +12,11 @@ all_sensory_cases = [True, False]
 #all_sensory_cases = [True]
 
 
-task_type = task_types[1]
+task_type = task_types[0]
 curriculum = curriculums[0]
 ANN_structure = ANN_structures[1]
 number_of_refinements = 9
-number_of_all_runs = 16
+number_of_all_runs = 50
 
 fig1, axes1 = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
 fig2, axes2 = plt.subplots(nrows=1, ncols=1, figsize=(6, 4.2))
@@ -24,7 +24,7 @@ fig2, axes2 = plt.subplots(nrows=1, ncols=1, figsize=(6, 4.2))
 color = "C0"
 for use_sensory in all_sensory_cases:
 	if use_sensory:
-		linestyle = ".-"
+		linestyle = ".:"
 		hatch = ""
 		experiment_ID = "w_sensory_"+ANN_structure+"_ANN_"+task_type+curriculum
 	else:
@@ -59,14 +59,25 @@ for use_sensory in all_sensory_cases:
 	task_errors_all_std = np.std(task_errors_all, axis=0)
 
 	for ii in range(3):
-		axes1[ii].errorbar(x=np.arange(number_of_refinements+1), y=learning_errors_all_mean[ii], yerr=learning_errors_all_std[ii], capsize=2, animated=True, alpha=.3, color=color)
-		axes1[ii].plot(np.arange(number_of_refinements+1), learning_errors_all_mean[ii],linestyle, alpha=.7, color=color)
+		if ii == 0:
+			#import pdb; pdb.set_trace()
+			axes1[ii].errorbar(x=np.arange(1,number_of_refinements+1), y=learning_errors_all_mean[ii,1:], yerr=learning_errors_all_std[ii,1:], capsize=2, animated=True, alpha=.4, color=color)
+			axes1[ii].plot(np.arange(1,number_of_refinements+1), learning_errors_all_mean[ii,1:],linestyle, alpha=.8, color=color)
+		else:
+			axes1[ii].errorbar(x=np.arange(number_of_refinements+1), y=learning_errors_all_mean[ii], yerr=learning_errors_all_std[ii], capsize=2, animated=True, alpha=.4, color=color)
+			if use_sensory:
+				line1, = axes1[ii].plot(np.arange(number_of_refinements+1), learning_errors_all_mean[ii],linestyle, alpha=.8, color=color)
+			else:
+				line2, = axes1[ii].plot(np.arange(number_of_refinements+1), learning_errors_all_mean[ii],linestyle, alpha=.8, color=color)
 
+		line3 = axes1[ii].axvline(x=.5, color='r', linestyle='dashdot', linewidth=1.5, alpha=.25)
 		axes1[ii].set_title(MuJoCo_model_names_short[ii])
 		axes1[ii].set_xlabel('Refinement #')
 		axes1[ii].set_ylabel('RMSE')
-		axes1[ii].set_ylim(0.2, .77)
+		axes1[ii].set_ylim(0.2, .9)
+		axes1[ii].set_xlim(-0.50, 9.5)
 		axes1[ii].grid(color='k', linestyle=':', linewidth=.5)
+
 	fig1.suptitle('RMSE vs. Refinement # (learning)', fontsize=16)
 	fig1.subplots_adjust(left=0.06, bottom=0.12, right=.95, top=.85, wspace=.25, hspace=.20)
 	if use_sensory:
@@ -93,6 +104,8 @@ for use_sensory in all_sensory_cases:
 	axes2.grid(color='k', linestyle=':', linewidth=.5)
 	axes2.set_ylim(0.2, .7)
 	fig2.subplots_adjust(bottom=0.15, top=.92)
+axes1[2].legend((line1,line2),('with sensory','without sensory'))
+
 
 save_figures = False
 if save_figures:
@@ -123,3 +136,4 @@ if show_video:
 		_ = test_a_task(MuJoCo_model_name, save_log_path, run_no, Mj_render=True, use_sensory=use_sensory, task_type=task_type, ANN_structure=ANN_structure, dt=dt, actuation_type=actuation_type)
 
 #import pdb; pdb.set_trace()
+
