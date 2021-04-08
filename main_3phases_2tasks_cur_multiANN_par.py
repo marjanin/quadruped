8b2aeaf00966aef8565308f4b1dd2b157ff80fc8
Ali Marjaninejad <marjanin@usc.edu>
2021-04-07 17:51:52 -0700

    cur3_V5_TD_full_test_nonstiff_3 results are added

--- main_3phases_2tasks_cur_multiANN_par.py
+++ main_3phases_2tasks_cur_multiANN_par.py
@@ -6,7 +6,7 @@ import multiprocessing as mp
 from all_functions import *
 
 def L2_learn_quadruped_experiment(run_no):
-	experiment_ID_base = 'cur3_V5_TD_full_test_nonstiff_2'
+	experiment_ID_base = 'cur3_V5_TD_full_test_nonstiff_3'
 # Create target Directory if don't exist
 	dt=.005
 	if not os.path.exists('./results/'+experiment_ID_base):
