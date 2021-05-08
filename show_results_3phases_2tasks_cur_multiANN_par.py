06c6b832c1f90dbba6f43a52f96ecf359bd6a525
Ali Marjaninejad <marjanin@usc.edu>
2021-05-07 17:31:34 -0700

    renamed the show files

--- show_results_3phases_2tasks_cur_multiANN_par.py
+++ show_results_3phases_2tasks_cur_multiANN_par.py
@@ -1,17 +1,16 @@
 import numpy as np
 from matplotlib import pyplot as plt
-#from all_functions import *
+from all_functions import *
 
-experiment_ID_base = 'cur4_xmlVer11_TD_V1'#mc1 error -> replaced with MC0 copy
-
+experiment_ID_base = 'cur3_V5_TD_full_test_nonstiff_modifiedRoM_rigid_3phases_V2_5_test3'
 
 curriculums = ["_E2H", "_H2E"]
 ANN_structures = ["S","M"]
 task_types = ["cyclical", "p2p"]
 
 all_sensory_cases = [True, False]
-use_feedback = True
-use_acc = True
+use_feedback = False
+use_acc = False
 normalize = True
 
 
@@ -20,11 +19,11 @@ random_seed = 0
 
 task_type = task_types[0]
 curriculum = curriculums[0]
-ANN_structure = ANN_structures[0]
+ANN_structure = ANN_structures[1]
 number_of_refinements = 6+1
-number_of_all_runs = 30
+number_of_all_runs = 32
 
-fig1, axes1 = plt.subplots(nrows=1, ncols=4, figsize=(12, 4))
+fig1, axes1 = plt.subplots(nrows=1, ncols=3, figsize=(12, 4))
 fig2, axes2 = plt.subplots(nrows=1, ncols=1, figsize=(6, 4.2))
 
 color = "C0"
@@ -46,11 +45,11 @@ for use_sensory in all_sensory_cases:
 		color= "C1"
 
 	if curriculum == "_E2H":
-		MuJoCo_model_names = ["tendon_quadruped_ws_inair.xml", "tendon_quadruped_ws_onfloor.xml", "tendon_quadruped_ws_onfloorloaded1000.xml","tendon_quadruped_ws_onfloorloaded3000.xml"]
-		MuJoCo_model_names_short = ["In Air", "On Floor", "On Floor With Load",  "On Floor With Heavy Load"]
+		MuJoCo_model_names = ["tendon_quadruped_ws_inair.xml", "tendon_quadruped_ws_onfloor1000.xml", "tendon_quadruped_ws_onfloorloaded.xml"]
+		MuJoCo_model_names_short = ["In Air", "On Floor", "On Floor With Load"]
 	elif curriculum == "_H2E":
-		MuJoCo_model_names = ["tendon_quadruped_ws_onfloorloaded3000.xml","tendon_quadruped_ws_onfloorloaded1000.xml", "tendon_quadruped_ws_onfloor.xml", "tendon_quadruped_ws_inair.xml"]
-		MuJoCo_model_names_short = ["On Floor With Heavy Load", "On Floor with Load", "On Floor", "In Air"]
+		MuJoCo_model_names = ["tendon_quadruped_ws_onfloorloaded.xml", "tendon_quadruped_ws_onfloor1000.xml", "tendon_quadruped_ws_inair.xml"]
+		MuJoCo_model_names_short = ["On Floor with Load", "On Floor", "In Air"]
 	else:
 		ValueError("unacceptable curriculum")
 
@@ -70,7 +69,7 @@ for use_sensory in all_sensory_cases:
 	task_errors_all_mean = np.mean(task_errors_all, axis=0)
 	task_errors_all_std = np.std(task_errors_all, axis=0)
 
-	for ii in range(4):
+	for ii in range(3):
 		if ii == 0:
 			#import pdb; pdb.set_trace()
 			axes1[ii].errorbar(x=np.arange(1,number_of_refinements+1), y=learning_errors_all_mean[ii,1:], yerr=learning_errors_all_std[ii,1:], capsize=2, animated=True, alpha=.4, color=color)
@@ -86,7 +85,7 @@ for use_sensory in all_sensory_cases:
 		axes1[ii].set_title(MuJoCo_model_names_short[ii])
 		axes1[ii].set_xlabel('Refinement #')
 		axes1[ii].set_ylabel('RMSE')
-		axes1[ii].set_ylim(0, 0.4)
+		axes1[ii].set_ylim(0, 0.6)
 		axes1[ii].set_xlim(-0.50, 9.5)
 		axes1[ii].grid(color='k', linestyle=':', linewidth=.5)
 
@@ -104,17 +103,17 @@ for use_sensory in all_sensory_cases:
 	# 	axes2.set_ylabel('RMSE')
 	# 	axes2.grid(color='k', linestyle=':', linewidth=.5)
 	# fig2.subplots_adjust(bottom=0.12, top=.92)
-	positions=np.arange(4)+x_shift
+	positions=np.arange(3)+x_shift
 	axes2.boxplot(task_errors_all, positions=positions)
 axes2.set_title('RMSE vs. Task (test)')
 axes2.set_xlabel('Task')
-axes2.set_xticklabels(["w. sen.","w. sen.","w. sen.","w. sen.","w/o. sen.","w/o. sen.","w/o. sen.","w/o. sen."])
+axes2.set_xticklabels(["w. sen.","w. sen.","w. sen.","w/o. sen.","w/o. sen.","w/o. sen."])
 # Rotate the tick labels and set their alignment.
 plt.setp(axes2.get_xticklabels(), rotation=-45, ha="left",
          rotation_mode="anchor")
 axes2.set_ylabel('RMSE')
 axes2.grid(color='k', linestyle=':', linewidth=.5)
-axes2.set_ylim(0, 0.4)
+axes2.set_ylim(0, 0.6)
 fig2.subplots_adjust(bottom=0.15, top=.92)
 axes1[2].legend((line1,line2),('with sensory','without sensory'))
 
@@ -144,11 +143,11 @@ else:
 	else:
 		experiment_ID = "wo_sensory_"+"wo_feedback_"+ANN_structure+"_ANN_"+task_type+curriculum
 save_log_path = experiment_ID_base+"/"+experiment_ID
-run_no = 3
-MuJoCo_model_names = ["tendon_quadruped_ws_inair.xml", "tendon_quadruped_ws_onfloor.xml", "tendon_quadruped_ws_onfloorloaded.xml","tendon_quadruped_ws_onfloorloadedheavy.xml"]
+run_no = 0
+MuJoCo_model_names = ["tendon_quadruped_ws_inair.xml", "tendon_quadruped_ws_onfloor.xml", "tendon_quadruped_ws_onfloorloaded.xml"]
 if show_video:
 	for MuJoCo_model_name , ii in zip(MuJoCo_model_names, range(len(MuJoCo_model_names))):
-		test_run_RMSE = test_a_task(MuJoCo_model_name, save_log_path, run_no, Mj_render=True, random_seed=random_seed, use_sensory=use_sensory, use_feedback=use_feedback, normalize=normalize, plot_position_curves=True, task_type=task_type, ANN_structure=ANN_structure, dt=dt, actuation_type=actuation_type, use_acc=use_acc)
+		test_run_RMSE = test_a_task(MuJoCo_model_name, save_log_path, run_no, Mj_render=False, random_seed=random_seed, use_sensory=use_sensory, use_feedback=use_feedback, normalize=normalize, plot_position_curves=True, task_type=task_type, ANN_structure=ANN_structure, dt=dt, actuation_type=actuation_type, use_acc=use_acc)
 		print(MuJoCo_model_name,"RMSE: " ,test_run_RMSE)
 
 #import pdb; pdb.set_trace()
