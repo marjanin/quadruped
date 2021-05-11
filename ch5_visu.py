8dfd8de0579e9aed842d5856a48d7c50d929948e
Ali Marjaninejad <ali_mcbkair@Alis-MacBook-Air.local>
2021-05-10 21:48:48 -0700

    cur4_xmlVer11_TD_V1 figures for _E2H are now added

--- ch5_visu.py
+++ ch5_visu.py
@@ -3,56 +3,52 @@ from matplotlib import pyplot as plt
 import scipy.stats as stats
 from ch5_visu_functions import *
 #from all_functions import *
-
 experiment_ID_base = 'cur4_xmlVer11_TD_V1'#mc1 error -> replaced with MC0 copy
 
 
-curriculums = ["_E2H", "_H2E"]
+
+number_of_refinements = 6+1
+number_of_all_runs = 60
+
+curricula = ["_E2H", "_H2E"]
 ANN_structures = ["S","M"]
 task_types = ["cyclical", "p2p"]
 
-all_sensory_cases = [True, False]
-use_feedback = False
-use_acc = True
-normalize = True
-
-
-show_video = False
-random_seed = 0
-
 task_type = task_types[0]
-curriculum = curriculums[0]
+curriculum = curricula[0]
 ANN_structure = ANN_structures[0]
-number_of_refinements = 6+1
-number_of_all_runs = 60
-use_sensory=True
+use_feedback = 0
+use_sensory = 1
+
+comparison_name="task_{}_cur_{}_stru_{}_fb_{}_sensory_{}".format(
+	task_type,curriculum,ANN_structure,use_feedback,"var")
+
+labels=["w/o tact", "w tact"]
 
 [learning_errors_all_1, task_errors_all_1] =\
 loading_plotting_data_fcn(
 	experiment_ID_base='cur4_xmlVer11_TD_V1',
-	use_sensory=False,
-	use_feedback=False,
-	curriculum="_E2H",
-	task_type="cyclical",
-	ANN_structure="M",
+	use_sensory=0,
+	use_feedback=use_feedback,
+	curriculum=curriculum,
+	task_type=task_type,
+	ANN_structure=ANN_structure,
 	number_of_refinements=6+1,
 	number_of_all_runs=60)
 [learning_errors_all_2, task_errors_all_2] =\
 loading_plotting_data_fcn(
 	experiment_ID_base='cur4_xmlVer11_TD_V1',
-	use_sensory=False,
-	use_feedback=False,
-	curriculum="_E2H",
-	task_type="cyclical",
-	ANN_structure="M",
+	use_sensory=use_sensory,
+	use_feedback=use_feedback,
+	curriculum=curriculum,
+	task_type=task_type,
+	ANN_structure=ANN_structure,
 	number_of_refinements=6+1,
 	number_of_all_runs=60)
 # import pdb; pdb.set_trace()
-labels=["S", "M"]
 fig1=compare_learning_error_plots_fcn(learning_errors_all_1, learning_errors_all_2, labels, curriculum="_E2H")
 fig2=compare_task_error_plots_fcn(task_errors_all_1, task_errors_all_2, labels)
 #import pdb; pdb.set_trace()
-comparison_name="test"
 save_figures = True
 if save_figures:
 	dpi = 600
@@ -60,7 +56,7 @@ if save_figures:
 	fig1.savefig("./results/{}/{}_figure1.png".format(experiment_ID_base,comparison_name), dpi=dpi)
 	#fig2.subplots_adjust(bottom=.12, top=.92)
 	fig2.savefig("./results/{}/{}_figure2.png".format(experiment_ID_base,comparison_name), dpi=dpi)
-plt.show(block=True)
+plt.show(block=1)
 
 # #import pdb; pdb.set_trace()
 
