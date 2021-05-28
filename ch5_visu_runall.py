a55ace3feef792f07ac2578a2a5f25a134f959eb
Ali Marjaninejad <ali_mcbkair@Alis-Air.attlocal.net>
2021-05-28 12:39:13 -0700

    modified the ch5_visu_runall to run partial results of cur4_xmlVer11_TD_V4

--- ch5_visu_runall.py
+++ ch5_visu_runall.py
@@ -2,18 +2,20 @@ import numpy as np
 from matplotlib import pyplot as plt
 import scipy.stats as stats
 import time
+from os import mkdir, path
 from ch5_visu_functions import *
 #from all_functions import *
-experiment_ID_base = 'cur4_xmlVer11_TD_V2'#mc1 error -> replaced with MC0 copy
-
+experiment_ID_base = 'cur4_xmlVer11_TD_V4'
+if not path.exists("./results/{}/figures/".format(experiment_ID_base)):
+	mkdir("./results/{}/figures/".format(experiment_ID_base))
 number_of_refinements = 6+1
-number_of_all_runs = 60
+number_of_all_runs = 16
 curriculum="_E2H"
 
 all_cases={
-"ANN_structures":["S","M"],
-"task_types":["cyclical", "p2p"],
-"feedback_cases":[0,1],
+"ANN_structures":["M"],
+"task_types":["cyclical"],
+"feedback_cases":[0],
 "tactile_cases":[0,1]}
 
 case_to_run_1={
@@ -98,9 +100,9 @@ for current_key, current_value in all_cases.items():
 				if save_figures:
 					dpi = 150
 					# fig1.subplots_adjust(left=.06, bottom=.12, right=.96, top=.92, wspace=.30, hspace=.20)
-					fig1.savefig("./results/{}/{}_figure1.png".format(experiment_ID_base,comparison_name), dpi=dpi)
+					fig1.savefig("./results/{}/figures/{}_figure1.png".format(experiment_ID_base,comparison_name), dpi=dpi)
 					#fig2.subplots_adjust(bottom=.12, top=.92)
-					fig2.savefig("./results/{}/{}_figure2.png".format(experiment_ID_base,comparison_name), dpi=dpi)
+					fig2.savefig("./results/{}/figures/{}_figure2.png".format(experiment_ID_base,comparison_name), dpi=dpi)
 					# plt.show(block=1)
 					plt.close(fig1)
 					plt.close(fig2)
